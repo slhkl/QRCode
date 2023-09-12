62b1bc8144b89d82f28def3eed60bde0a4c6034f
Salih Kol <salih.kol@detaysoft.com>
2023-09-12 14:14:55 +0300

    Add project files.

---
+++ Api/Controllers/QRController.cs
@@ -0,0 +1,16 @@
+﻿using Business;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class QRController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult GenerateQRCode([FromBody] string value)
+        {
+            return base.File(value.ConvertToQR(), "image/png");
+        }
+    }
+}
---
+++ Business/Extension.cs
@@ -0,0 +1,27 @@
+﻿using QRCoder;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace Business
+{
+    public static class Extension
+    {
+        public static string ConvertToQRImage(this string value)
+        {
+            return Convert.ToBase64String(value.ConvertToQR());
+        }
+
+        public static byte[] ConvertToQR(this string value)
+        {
+            QRCodeGenerator qrGenerator = new QRCodeGenerator();
+            QRCodeData qrCodeData = qrGenerator.CreateQrCode(value, QRCodeGenerator.ECCLevel.Q);
+            QRCode qrCode = new QRCode(qrCodeData);
+
+            Bitmap qrDrawer = qrCode.GetGraphic(60);
+
+            using MemoryStream ms = new MemoryStream();
+            qrDrawer.Save(ms, ImageFormat.Png);
+            return ms.ToArray();
+        }
+    }
+}
